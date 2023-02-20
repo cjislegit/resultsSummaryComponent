8be8b37f7f1c51d06ce5438c5222a29f89a10cb1
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-02-20 15:46:47 -0800

    Started styling the score container

--- src/components/score/Score.js
+++ src/components/score/Score.js
@@ -1,7 +1,19 @@
 import styles from './Score.module.css';
 
 const Score = () => {
-    return ( <div className={styles.scoreContainer}>This is another test</div> );
-}
- 
+  return (
+    <div className={styles.scoreContainer}>
+      <div className={styles.title}>Your Result</div>
+      <div className={styles.score}>
+        <div>76</div>
+        <div>of 100</div>
+      </div>
+      <div className={styles.result}>Great</div>
+      <div className={styles.summary}>
+        You scored higher than 65% of the people who have taken this test.
+      </div>
+    </div>
+  );
+};
+
 export default Score;
