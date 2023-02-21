94b6f1faa8ca3cc39438951fcbd1e523a9dc6c9b
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-02-20 19:47:57 -0800

    Set up files for summary component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import Score from './components/score/Score';
+import Summary from './components/summary/Summary';
 
 //Fonts
-import "./assets/fonts/HankenGrotesk-VariableFont_wght.ttf";
+import './assets/fonts/HankenGrotesk-VariableFont_wght.ttf';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Score />
+      <Summary />
     </div>
   );
 }
---
+++ src/components/summary/Summary.js
@@ -0,0 +1,7 @@
+import styles from './Summary.module.css';
+
+const Summary = () => {
+  return <div className={styles.summaryContainer}></div>;
+};
+
+export default Summary;
