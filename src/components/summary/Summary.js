f18fbc30843e9436a4343f760aa0ed50e5e46b79
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-02-24 17:58:00 -0800

    scores are now pulled in from json file instead of hardcoded

--- src/components/summary/Summary.js
+++ src/components/summary/Summary.js
@@ -1,54 +1,27 @@
 import styles from './Summary.module.css';
-import reaction from '../../assets/images/icon-reaction.svg';
-import memory from '../../assets/images/icon-memory.svg';
-import verbal from '../../assets/images/icon-verbal.svg';
-import visual from '../../assets/images/icon-visual.svg';
+import data from '../../data.json';
 
 const Summary = () => {
   return (
     <div className={styles.summaryContainer}>
       <div className={styles.title}>Summary</div>
       <div className={styles.scores}>
-        <div className={`${styles.score} ${styles.red}`}>
-          <div>
-            <img src={reaction} alt='Lightning bolt icon' />
-            Reaction
-          </div>
-          <div>
-            <span>80</span>
-            <span>/ 100</span>
-          </div>
-        </div>
-        <div className={`${styles.score} ${styles.yellow}`}>
-          <div>
-            <img src={memory} alt='Brain icon' />
-            Memory
-          </div>
-          <div>
-            <span>92</span>
-            <span>/ 100</span>
-          </div>
-        </div>
-        <div className={`${styles.score} ${styles.green}`}>
-          <div>
-            <img src={verbal} alt='Chat bubble icon' />
-            Verbal
-          </div>
-          <div>
-            <span>61</span>
-            <span>/ 100</span>
-          </div>
-        </div>
-        <div className={`${styles.score} ${styles.blue}`}>
-          <div>
-            <img src={visual} alt='Eye ball icon' />
-            Visual
-          </div>
-          <div>
-            <span>71</span>
-            <span>/ 100</span>
-          </div>
-        </div>
+        {data.map((score) => {
+          let icon = require(`../../${score.icon}`);
+          let style = score.style;
+          return (
+            <div className={`${styles.score} ${styles[style]}`} key={style}>
+              <div>
+                <img src={icon} alt={score.alt} />
+                {score.category}
+              </div>
+              <div>
+                <span>{score.score}</span>
+                <span>/ 100</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
       <div className={styles.button}>
         <input type='button' value='Continue' />
